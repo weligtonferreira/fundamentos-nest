986ec003f3811bdf0adb90c3ca2ae866f3cde149
Weligton <weligton0ferreira@gmail.com>
2026-03-23 22:02:01 -0300

    feat(AppModule): configure database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
+import { UserService } from './user/user.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      synchronize: false,
+      migrationsRun: true,
+      entities: [`${__dirname}/**/*.entity{.js,.ts}`],
+      migrations: [`${__dirname}/database/migrations/{.ts,*.js}`],
+    }),
+    UserModule,
+  ],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
