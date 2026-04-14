caa4edab171242706f7acc8fadebcd102b334ab7
Weligton <weligton0ferreira@gmail.com>
2026-04-13 22:11:02 -0300

    fix(AppModule): remove User dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
-import { UserService } from './user/user.service';
 
 @Module({
   imports: [
@@ -27,7 +25,7 @@ import { UserService } from './user/user.service';
     }),
     UserModule,
   ],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
