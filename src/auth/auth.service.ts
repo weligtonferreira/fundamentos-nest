a8c29c2ba8b7e9ecf704843c7f708cb0ec91e97b
Weligton <weligton0ferreira@gmail.com>
2026-05-25 20:27:55 -0300

    refactor(AuthService): resolve import of JetTokenResponseDto

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 
 import { UserService } from '../user/user.service';
+import { JwtTokenReponseDto } from './dto/jwt-token-response.dto';
 
 @Injectable()
 export class AuthService {
