d94de773f637a8be7fecbc5812e2fd81ac1ab333
Weligton <weligton0ferreira@gmail.com>
2026-05-26 09:21:00 -0300

    feat(UserController): add Public decorator in create method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -15,11 +15,13 @@ import {
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Public } from '../auth/public.decorator';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Public()
   @Post()
   @UsePipes(new ValidationPipe({ transform: true }))
   create(@Body() createUserDto: CreateUserDto) {
