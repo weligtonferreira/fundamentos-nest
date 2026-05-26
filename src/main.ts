5a27578a26119af3501a8ddcc5afd2bf4c500256
Weligton <weligton0ferreira@gmail.com>
2026-05-26 09:12:02 -0300

    feat(main): enable cors configuration

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    credentials: true,
+    origin: [process.env.FRONTEND_URL],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
